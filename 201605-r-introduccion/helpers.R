18100897726f58b2b6dbe43a44282d11fc52caa8
Joshua <jbkunst@gmail.com>
2016-05-11 17:58:12 -0300

    updating

--- 201605-r-introduccion/helpers.R
+++ 201605-r-introduccion/helpers.R
@@ -4,11 +4,15 @@ knit_print.htmlwidget <- function(x, ..., options = NULL){
   
   wdgtclass <- setdiff(class(x), "htmlwidget")[1]
   wdgtrndnm <- paste0(sample(letters, size = 7), collapse = "")
-  wdgtfname <- sprintf("wdgts/%s_%s.html", wdgtclass, wdgtrndnm)
+  wdgtfname <- sprintf("%s_%s.html", wdgtclass, wdgtrndnm)
   
   htmlwidgets::saveWidget(x, file = wdgtfname, selfcontained = TRUE, background = "transparent")
   
-  iframetxt <- sprintf("<iframe  frameBorder=\"0\" src=\"%s\" width=\"100%%\"   height=\"600\"></iframe>", wdgtfname)
+  file.copy(wdgtfname, file.path("wdgts", wdgtfname))
+  file.remove(wdgtfname)
+  
+  iframetxt <- sprintf("<iframe  frameBorder=\"0\" src=\"%s\" width=\"100%%\"   height=\"600\"></iframe>",
+                       file.path("wdgts", wdgtfname))
   
   knitr::asis_output(iframetxt)
 }

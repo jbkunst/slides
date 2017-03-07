0d17aa090f7cb4f40a41c363ecce54a61a8bbc6c
Joshua <jbkunst@gmail.com>
2017-03-07 18:37:34 -0300

    yuju

--- 201703-DataVisualizationMeetup/code.R
+++ 201703-DataVisualizationMeetup/code.R
@@ -1,39 +1,41 @@
-#' ---
-#' title: ""
-#' author: "Joshua Kunst"
-#' output:
-#'  html_document:
-#'    toc: true
-#'    keep_md: yes
-#' ---
-
-#+ echo=FALSE, message=FALSE, warning=FALSE
-#### setup ws packages ####
+# ws & packages -----------------------------------------------------------
 rm(list = ls())
-knitr::opts_chunk$set(message = FALSE, warning = FALSE)
+
+# general
 library(tidyverse)
 library(stringr)
+library(lubridate)
+
+# cors
 library(widyr) # devtools::install_github("dgrtwo/widyr")
 library(igraph)
+
+# autoencoder
+library(h2o)
+localH2O <- h2o.init(nthreads = -2)
+
+# viz
 library(highcharter)
+library(viridis)
+library(hrbrthemes)
+library(jbkmisc)
+theme_set(theme_jbk())
 options(highcharter.theme = hc_theme_elementary())
-#'
-#' Read data
-#' 
+
+jbkmisc::wf_create_folders()
+
+# downloadload data -------------------------------------------------------
 #' http://www.dtpm.gob.cl/index.php/2013-04-29-20-33-57/matrices-de-viaje
-
-
-
 urlfile <- "http://www.dtpm.gob.cl/descargas/archivos/2015.04_Subidas_paradero_mediahora_web.rar"
 rarfile <- file.path("data", basename(urlfile))
 
 if(!file.exists(rarfile)) {
-  dir.create("data")
   download.file(urlfile, file.path("data", basename(urlfile)), mode = "wb")
 }
 
 data <- read_csv2("data/2015.04_Subidas_paradero_mediahora_web/2015.04_Subidas_paradero_mediahora_web.csv")
- 
+
+# data cleaning -----------------------------------------------------------
 data <- data %>% 
   mutate(subidas_laboral_promedio = as.numeric(subidas_laboral_promedio)) %>% 
   filter(!str_detect(paraderosubida, "^(T|L|I|E)?-")) %>% 
@@ -50,60 +52,55 @@ data <- complete(data, paraderosubida, mediahora,
 data <- filter(data, mediahora != 0)
 
 glimpse(data)
+saveRDS(data, "data/data_subidas_metro.rds")
 
-#' ## Correlation
-
-dcor <- data %>%
-  pairwise_cor(paraderosubida, mediahora, subidas_laboral_promedio,
-               upper = FALSE) %>% 
-  arrange(desc(correlation))
-
-head(dcor)
-
-dcor1 <- data %>% 
-  filter(paraderosubida %in% c("Plaza Maipu", "Laguna Sur")) 
-
+# explore -----------------------------------------------------------------
+d1 <- filter(data, paraderosubida %in% c("Plaza Maipu", "Laguna Sur")) 
 hw_grid(
-  dcor1 %>% 
+  d1 %>% 
     hchart("line", hcaes(mediahora, subidas_laboral_promedio, group = paraderosubida)) %>% 
     hc_xAxis(type = "datetime") %>% 
     hc_tooltip(sort = TRUE, table = TRUE, xDateFormat = "%H:%S"),
-  dcor1 %>% 
+  d1 %>% 
     spread(paraderosubida, subidas_laboral_promedio) %>% 
     hchart("point", hcaes(`Laguna Sur`, `Plaza Maipu`, size = mediahora), maxSize = "5%") %>% 
     hc_xAxis(min = 0) %>% 
     hc_yAxis(min = 0)
 ) %>% htmltools::browsable()
 
-
-tail(dcor)
-dcor2 <- data %>% 
-  filter(paraderosubida %in% c("Universidad De Chile", "Plaza De Puente Alto"))
-
+d2 <- filter(data, paraderosubida %in% c("Universidad De Chile", "Plaza De Puente Alto"))
 hw_grid(
-  dcor2 %>% 
+  d2 %>% 
     hchart("line", hcaes(mediahora, subidas_laboral_promedio, group = paraderosubida)) %>% 
     hc_xAxis(type = "datetime") %>% 
     hc_tooltip(sort = TRUE, table = TRUE, xDateFormat = "%H:%S"),
-  dcor2 %>% 
+  d2 %>% 
     spread(paraderosubida, subidas_laboral_promedio) %>% 
     hchart("point", hcaes(`Plaza De Puente Alto`, `Universidad De Chile`, size = mediahora), maxSize = "5%") %>% 
     hc_xAxis(min = 0) %>% 
     hc_yAxis(min = 0)
 ) %>% htmltools::browsable()
 
-rm(dcor1, dcor2)
+rm(d1, d2)
 
+# correlations ------------------------------------------------------------
+dcor <- data %>%
+  pairwise_cor(paraderosubida, mediahora, subidas_laboral_promedio,
+               upper = FALSE) %>% 
+  arrange(desc(correlation))
+
+dcor
+
+saveRDS(dcor, "data/data_subidas_metro_cor.rds")
+
+# network -----------------------------------------------------------------
 dcorf <- dcor %>%
   arrange(desc(correlation)) %>%
   filter(row_number() <= 400)
 
-# dcorf <- dcorf %>% 
-#   group_by(item1) %>% 
-#   filter(row_number() <= 2) %>% 
-#   ungroup() 
-
 g <- graph_from_data_frame(dcorf, directed = FALSE)
+hchart(g)
+g
 
 E(g)$weight <- dcorf$correlation^2
 
@@ -143,10 +140,11 @@ V(g)$tendencia <- round(dvert$tend, 2)
 # V(g)$color <- colorize(dvert$tend)
 V(g)$color <- colorize(dvert$comm)
 
+saveRDS(g, "data/data_g.rds")
+
 
 set.seed(1)
 hchart(g) %>% 
-  hc_tooltip(valueDecimals = 2) %>% 
   hc_add_theme(
     hc_theme_elementary(
       yAxis = list(visible = FALSE),
@@ -155,22 +153,79 @@ hchart(g) %>%
   )
 
 
-# ex ----------------------------------------------------------------------
-left_join(data, dvert) %>% 
-  mutate(comm = ifelse(is.na(comm), "Sin com", comm)) %>% 
-  ggplot(aes(mediahora, subidas_laboral_promedio, group = paraderosubida, color = comm)) +
-  geom_line(alpha = 0.2) +
-  geom_smooth(aes(group = comm)) + 
-  # scale_x_date() +
-  facet_wrap(~comm, ncol = 1, scales = "free_y")
-
-#' ## Autoencoder
+# autoencoder -------------------------------------------------------------
 data2 <- data %>% 
-  group_by(paraderosubida) %>% 
-  mutate(subidas_laboral_promedio = scale(subidas_laboral_promedio),
-         mediahora = paste0("m", mediahora)) %>% 
+  mutate(mediahora = paste0("m", mediahora/1e3)) %>% 
   ungroup() %>% 
-  spread(mediahora, subidas_laboral_promedio) 
-
+  spread(mediahora, subidas_laboral_promedio)
 data2
 
+saveRDS(data2, "data/data_subidas_metro_autoencoder.rds")
+
+dh2o <- as.h2o(data2)
+
+mod_autoenc <- h2o.deeplearning(
+  x = names(dh2o)[-1],
+  training_frame = dh2o,
+  hidden = c(400, 200, 2, 200, 400),
+  epochs = 50,
+  activation = "Tanh",
+  autoencoder = TRUE
+)
+
+dautoenc <- h2o.deepfeatures(mod_autoenc, dh2o, layer = 3) %>% 
+  as.data.frame() %>% 
+  tbl_df() %>% 
+  setNames(c("x", "y")) %>% 
+  mutate(paraderosubida = data2$paraderosubida)
+
+
+saveRDS(dautoenc, "data/data_subidas_metro_autoencoder_output.rds")
+
+hchart(dautoenc, "point", hcaes(x = x, y = y, name = paraderosubida),
+       dataLabels = list(enabled = TRUE, format =  "{point.name}"))
+  
+dkmod <- map_df(1:15, function(k){
+  mod.km <- h2o.kmeans(training_frame = as.h2o(dautoenc), k = k, x = c("x", "y"))  
+  mod.km@model$model_summary
+})
+
+dkmod <- dkmod %>%
+  mutate(wc_ss = within_cluster_sum_of_squares/total_sum_of_squares,
+         bt_ss = between_cluster_sum_of_squares/total_sum_of_squares)
+
+hchart(dkmod, "line", hcaes(number_of_clusters, wc_ss), name = "WC SS")
+
+mod_km <- h2o.kmeans(training_frame = as.h2o(dautoenc), k = 4, x = c("x", "y"))  
+
+dautoenc <- dautoenc %>% 
+  mutate(group = as.vector(h2o.predict(object = mod_km, newdata = as.h2o(.))),
+         group = as.numeric(group) + 1,
+         group = paste("group", group))
+
+saveRDS(dautoenc, "data/data_subidas_metro_autoencoder_output_km.rds")
+
+hchart(dautoenc, "point", hcaes(x, y, group = group, name = paraderosubida)) %>% 
+  hc_colors(hex_to_rgba(viridis(4)))
+
+data <- left_join(data, select(dautoenc, paraderosubida, group))
+
+data <- data %>% 
+  mutate(mh = as.POSIXct(mediahora/1000, origin="1970-01-01"),
+         mh = mh + hours(2) + minutes(30))
+
+saveRDS(data, "data/data_subidas_metro_km.rds")
+
+ggplot(data, aes(mh, subidas_laboral_promedio)) + 
+  geom_line(aes(group = paraderosubida), alpha = 0.1) + 
+  geom_smooth(aes(group = group, color = group), line = 1.2, se = FALSE) + 
+  scale_color_viridis(discrete = TRUE) +
+  scale_y_comma() + 
+  scale_x_datetime(date_labels = "%H:%M") + 
+  facet_wrap(~group, scales = "free_x")  +
+  theme(legend.position = "bottom") + 
+  labs(
+    x = "Hora", y = "Subida promedio",
+    caption = "jkunst.com"
+  )
+ggsave("trendgroup.png", width = 16, height = 9)

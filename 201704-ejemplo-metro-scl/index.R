cb3368cfe90f3587110d40f2868bfc46d7cfc34a
Joshua <jbkunst@gmail.com>
2017-04-13 18:34:26 -0300

    adding index

---
+++ 201704-ejemplo-metro-scl/index.R
@@ -0,0 +1,21 @@
+#' ---
+#' title: "Ejemplo Metro SantiagoCL"
+#' author: "Joshua Kunst"
+#' output:
+#'  html_document:
+#'    theme: yeti
+#'    toc: true
+#'    toc_float: true
+#' ---
+#' 
+#' ## Introducción
+#' 
+#' asd asd
+#' 
+#' ## Requisitos
+#' 
+#' - conocer readr, dplyr y magrittr
+#' -
+#' 
+#' ## Cargando Paquetes y Datos
+

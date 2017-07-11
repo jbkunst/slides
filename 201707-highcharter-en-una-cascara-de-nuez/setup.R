6ec5ce619679df3995ed4c9100b90026c7b99ca3
Joshua <jbkunst@gmail.com>
2017-07-11 13:03:50 -0400

    taking speed

--- 201707-highcharter-en-una-cascara-de-nuez/setup.R
+++ 201707-highcharter-en-una-cascara-de-nuez/setup.R
@@ -1,4 +1,10 @@
 library(tidyverse)
 library(highcharter)
 library(forecast)
+library(shiny)
 options(highcharter.theme = hc_theme_smpl(chart = list(backgroundColor = "transparent")))
+
+ico <- function(x = "tv", color = NULL) {
+  # color <- "red"
+  as.character(tags$span(icon(x), style = sprintf("color:%s", color)))
+}

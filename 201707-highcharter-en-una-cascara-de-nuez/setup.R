4879877527520db4fc2ad59c2a1cc4cd53c9158d
Joshua <jbkunst@gmail.com>
2017-07-11 18:44:11 -0400

    gogogogog plzplzplz

--- 201707-highcharter-en-una-cascara-de-nuez/setup.R
+++ 201707-highcharter-en-una-cascara-de-nuez/setup.R
@@ -2,7 +2,10 @@ library(tidyverse)
 library(highcharter)
 library(forecast)
 library(shiny)
+
+knitr::opts_chunk$set(message = FALSE, warning = FALSE, echo = TRUE)
 options(highcharter.theme = hc_theme_smpl(chart = list(backgroundColor = "transparent")))
+
 
 ico <- function(x = "tv", color = NULL) {
   # color <- "red"

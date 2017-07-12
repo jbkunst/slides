a9160592668b241922563703d289ea1b82e959bf
Joshua <jbkunst@gmail.com>
2017-07-12 18:26:38 -0400

    updating asd!

--- 201707-highcharter-en-una-cascara-de-nuez/setup.R
+++ 201707-highcharter-en-una-cascara-de-nuez/setup.R
@@ -1,6 +1,4 @@
-library(tidyverse)
 library(highcharter)
-library(forecast)
 library(shiny)
 
 knitr::opts_chunk$set(message = FALSE, warning = FALSE, echo = TRUE)
@@ -10,4 +8,8 @@ options(highcharter.theme = hc_theme_smpl(chart = list(backgroundColor = "transp
 ico <- function(x = "tv", color = NULL) {
   # color <- "red"
   as.character(tags$span(icon(x), style = sprintf("color:%s", color)))
+}
+
+R <- function() {
+  as.character(tags$span("R", style = "color:#2066B9;font-weight:500"))
 }
